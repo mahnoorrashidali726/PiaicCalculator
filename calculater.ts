45947ebb1b5314ce0d30310dac49a3826116ae94
mahnoorrashidali726 <mahnoorrashidali726@gmail.com>
2023-09-25 23:31:02 -0700

    first commit

---
+++ calculater.ts
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+const answers:{
+    num1:number,
+    num2:number,
+   operator:string
+}=await inquirer.prompt([{
+    type:"number",
+    name:"num1",
+    message:"Please enter your first no:"
+},
+{
+    type:"number",
+    name:"num2",
+    message:"Please enter your second no:"
+},
+{
+type:"list",
+name:"operator",
+choices:["-","+","*","/"],
+message:"Please select your operator:"
+}])
+const {num1,num2,operator} = answers;
+if(num1 && num2 && operator){
+    let result :number = 0;
+    if(operator == "-"){
+        result = num1 - num2;
+    }
+       else if(operator == "+"){
+        result = num1 +num2;
+    }
+    else if(operator == "/"){
+        result = num1 / num2;
+    } 
+    else if(operator == "*"){
+        result = num1 * num2;
+    }
+    console.log(`Your result is:` , result );
+}else{
+    console.log("Please enter vilad input.")
+}
